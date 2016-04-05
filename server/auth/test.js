798b64d5af1ed9412defee6d0662fc9b037f1f96
lovelydreamer <christinamitchell08@gmail.com>
2016-04-05 13:05:01 -0500

    template for auth started

--- server/auth/test.js
+++ server/auth/test.js
@@ -1,3 +1,13 @@
-/*
-STUFF
-*/
+var passport = require('passport');
+
+
+// AUTHENTICATE
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+ 	 // if auth fails, passport responds w/ 401 unauth user by default
