2895207580ac4a8e00fcbf5c1765ada52d38e047
bluestreek18 <bluestreek@gmail.com>
2016-04-13 12:10:23 -0500

    trying to merge new changes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-//var browserify = require('browserify-middleware')
 var express = require('express')
 var webpack = require ('webpack');
-var webpackDevMiddleware = require ('webpack-dev-middleware')  
-//var webpackHotMiddleware = require ('webpack-hot-middleware')  
-var config = require( './../webpack.config.js')
-var compiler = webpack(config)  
+var webpackDevMiddleware = require ('webpack-dev-middleware');
+var config = require( './../webpack.config.js');
+var compiler = webpack(config);
 
 var Path = require('path')
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var passport = require('passport')
+var passport = require('passport');
 var flash    = require('connect-flash'); // messages stored in session
 
 var Posts = require('./../client/models/posts');
 var Users = require('./../client/models/users');
 
-var routes = express.Router()
-var app = express()
+var routes = express.Router();
+var app = express();
 
 
 app.use(webpackDevMiddleware(compiler, {  
@@ -26,16 +24,12 @@ app.use(webpackDevMiddleware(compiler, {
     stats: {colors: true}  
 }))
 
-//app.use(webpackHotMiddleware(compiler, {  
-//    log: console.log 
-//})) 
-// Parse incoming request bodies as JSON
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 // Mount our main router
+// First place requests come!
 app.use('/', routes)
 
 
